1d57a8ba015c51dabecf63669bb593a66eae789d
MaxMillington <maxmillington89@gmail.com>
2015-09-14 14:17:42 -0600

    initial commit for the Rails Engine project. So far, I have imported the CSV files, completed all the migrations, and am currently working on setting up the rake file to load all the CSV data

---
+++ db/migrate/20150914195040_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :description
+      t.decimal :unit_price, precision: 8, scale: 2
+      t.references :merchant, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20150914195257_create_invoice_items.rb
@@ -0,0 +1,12 @@
+class CreateInvoiceItems < ActiveRecord::Migration
+  def change
+    create_table :invoice_items do |t|
+      t.integer :quantity
+      t.decimal :unit_price, precision: 8, scale: 2
+      t.references :invoice, index: true, foreign_key: true
+      t.references :item, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
