0e8cbb69163dc931c3e56f1cbad324f394b631ea
MaxMillington <maxmillington89@gmail.com>
2015-09-15 17:33:20 -0600

    added most revenue method for merchants and corresponding controller test

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,9 +5,14 @@ class Merchant < ActiveRecord::Base
   has_many :invoices
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
-  
-  def self.most_revenue(params)
 
+  def self.most_revenue(params)
+    sorted_merchants = all.sort_by do |merchant|
+      merchant.invoices.successful.joins(:invoice_items).
+          sum('quantity * unit_price')
+    end
+
+    sorted_merchants.reverse[0...(params[:quantity]).to_i]
   end
 
   def self.most_items(params)
