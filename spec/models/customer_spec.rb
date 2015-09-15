0282b3ac8abb6bbe03c65d54e52d3b4bc3605940
MaxMillington <maxmillington89@gmail.com>
2015-09-14 20:43:26 -0600

    added model test for customers

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:customer) { Customer.create(first_name: 'Django',
+                                   last_name: 'Reinhardt') }
+  let(:merchant) { Merchant.create(name: 'Max the Merchant') }
+  let(:item) { Item.create(name: 'Monkeys',
+                           description: 'Be careful with these mischievous monkeys.',
+                           unit_price: 67.99, merchant_id: merchant.id) }
+  let(:invoice) { Invoice.create(customer_id: customer.id, merchant_id: merchant.id, status: "paid")}
+
+  it 'is valid' do
+    expect(customer).to be_valid
+  end
+
+  it 'is invalid without a first name' do
+    customer.first_name = nil
+    expect(customer).to_not be_valid
+  end
+
+  it 'is invalid without a last name' do
+    customer.last_name = nil
+    expect(customer).to_not be_valid
+  end
+
+  it 'returns the merchant' do
+    Merchant.create(name: 'Max the Merchant')
+    Invoice.create(customer_id: customer.id,
+                   merchant_id: merchant.id, status: "paid")
+    expect(customer.merchants.first).to eq(merchant)
+  end
+
+  it 'returns the invoice' do
+    Merchant.create(name: 'Charles')
+    invoice2 = Invoice.create(customer_id: customer.id,
+                   merchant_id: merchant.id, status: "paid")
+    expect(customer.invoices.first).to eq(invoice2)
+  end
 end
