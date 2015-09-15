c075f25819e9884efff7ac00046e72b0d55b903a
MaxMillington <maxmillington89@gmail.com>
2015-09-14 20:27:06 -0600

    figured out why my find by name merchant wasn't working in postman and fixed it.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -12,11 +12,11 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def find
-    respond_with Merchant.find_by(id: params[:id])
+    respond_with Merchant.find_by(find_params)
   end
 
   def find_all
-    respond_with Merchant.where(id: params[:id])
+    respond_with Merchant.where(find_params)
   end
 
   private
